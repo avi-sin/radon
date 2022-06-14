e8d408ee0033f2355fbadcf30876b84d4642a315
Avinash Singh <avinashs.iitm@gmail.com>
2022-06-14 23:46:50 +0530

    jwt assignment

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://users-open-to-all:hiPassword123@cluster0.uh35t.mongodb.net/skDB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://avi-sin:CJTIF4CupXQdRKHV@cluster0.ovf3r.mongodb.net/auth-1", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
