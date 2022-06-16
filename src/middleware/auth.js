3f59b63f571005a4f4623962146abea243d119d1
Avinash Singh <avinashs.iitm@gmail.com>
2022-06-16 23:22:25 +0530

    urgent commit

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -3,49 +3,38 @@ const jwt = require("jsonwebtoken");
 let decodedToken
 
 const authenticate = async function (req, res, next) {
-    let token = req.headers["x-Auth-token"] || req.headers["x-auth-token"]
 
-    //If no token is present in the request header return error
-    if (!token) return res.send({ status: false, msg: "token must be present in the request header" });
+    try{
+        let token = req.headers["x-Auth-token"] || req.headers["x-auth-token"]
+        if (!token) return res.status(403).send({ status: false, msg: "token must be present in the request header" });
 
-    // ================================================================================= //
-    decodedToken = jwt.verify(token, 'functionup-radon')
-    
-    if(!decodedToken) return res.send({status: false, msg:"token is not valid"})
-    // ================================================================================= //
-    
-    /*
+        decodedToken = jwt.verify(token, 'functionup-radon')
+        next()
 
-     jwt.verify(token, 'functionup-radon', (err,decode)=>{
-        if(err){
-            res.send({message:err.message})
-        }else{
-            req.decodedToken=decode
-        }
-     })
+    } catch (error) {
+        res.status(500).send({error: error.message})
+    }
 
-    */
-
-    next()
 }
 
 
 
-
 const authorize = async function (req, res, next) {
-    let userRequested = req.params.userId
 
-    //userId for the logged-in user
+    try{
+        let userRequested = req.params.userId
+        let userLoggedIn = decodedToken.userId
 
-    let userLoggedIn = decodedToken.userId
-    // let userLoggedIn = req.decodedToken.userId
+        if (userRequested != userLoggedIn) {
+            return res.status(401).send({ status: false, msg: "Logged-in user is not allowed to access the requested user's data" })
+        } else {
+            next()
+        }
 
-    //userId comparision to check if the logged-in user is requesting for their own data
-    if (userRequested != userLoggedIn) {
-        return res.send({ status: false, msg: "Logged-in user is not allowed to access the requested user's data" })
-    } else {
-        next()
+    } catch (error) {
+        res.status(500).send({error: error.message})
     }
+    
 }
 
 
