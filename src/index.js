1066617f276228aedf426be12af2bf4ec50a39ac
avi-sin <106535325+avi-sin@users.noreply.github.com>
2022-11-10 21:57:16 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ const mongoose = require('mongoose');
 const routes = require('./routes');
 const app = express();
 app.use(express.json());
-// const {studentModel, adminModel} = require('./studentModel');
 
 mongoose.connect('mongodb+srv://avi-sin:CJTIF4CupXQdRKHV@cluster0.ovf3r.mongodb.net/assignment1')
 .then(() => console.log('MongoDB is connected...'), (err) => console.log(err));
